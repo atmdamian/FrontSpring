58ce31883534d748c334d8904c00b25f1811329d
Miguel Herrera <mherrera@promad.com.mx>
2020-12-10 00:08:31 -0600

    tabla usuarios

--- src/main/java/com/softtek/web/app/controller/UsuarioController.java
+++ src/main/java/com/softtek/web/app/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.softtek.web.app.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -10,21 +13,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.softtek.web.app.model.Usuario;
 import com.softtek.web.app.response.RequestStatus;
 
-@Controller
-@RequestMapping("/usuario")
+@RestController
+@RequestMapping("api/usuario")
 public class UsuarioController {
-
-	@GetMapping
-	public String viewUsuario(Map<String, String> map) {
-		map.put("titulo", "registro");
-		return "registrar";
-	}
 
 	@PostMapping(value = "/agregar")
 	public @ResponseBody ResponseEntity<?> agregarUsuario(@ModelAttribute Usuario usuario) {
@@ -44,5 +46,17 @@ public class UsuarioController {
 		return new ResponseEntity<String>("ERROR", HttpStatus.BAD_REQUEST);
 
 	}
+	
+	@GetMapping(value = "/obtenerUsuarios")
+	public List<Usuario> obtenerUsuarios(Map<String, String> map) throws JsonMappingException, RestClientException, JsonProcessingException {
+		List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+		ObjectMapper mapper = new ObjectMapper();
+		RestTemplate rest = new RestTemplate();
+			listaUsuarios = Arrays.asList(mapper.readValue(
+			rest.getForObject("http://localhost:8081/api/usuario/allUsers", String.class), Usuario[].class));
+
+		return listaUsuarios;
+	}
 
 }
+
---
+++ src/main/java/com/softtek/web/app/controller/ViewUsuarioController.java
@@ -0,0 +1,19 @@
+package com.softtek.web.app.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
+public class ViewUsuarioController {
+	
+	@GetMapping
+	public String viewUsuario(Map<String, String> map) {
+		map.put("titulo", "registro");
+		return "registrar";
+	}
+
+}
